5ec570570e6284053dfe425709650c13c162e5bf
brazillucas <lcardoso64@hotmai.com>
2022-11-18 16:59:52 -0300

    Iniciando métodos privados

---
+++ Métodos/Carro/Carro.java
@@ -0,0 +1,53 @@
+// Carro(combustivel: double)
+// percorrerDistancia(int distancia): void
+// abastecer(listros: double): void
+// automnomia(): double
+// getCombustivel(): double
+// setCombustivel(double combustivel): void
+// getQuilometragem(): double
+// setQuilometragem(double quilometros): void
+
+public class Carro {
+    private double combustivel;
+    private double quilometragem;
+
+    public Carro (double combustivel, double quilometragem) {
+        this.combustivel = combustivel;
+        this.quilometragem = quilometragem;
+    }
+
+    public void percorrerDistancia (int distancia) {
+
+    }
+
+    public void abastecer (double litros) {
+
+    }
+
+    public double automnomia () {
+        
+        return combustivel;
+
+    }
+
+    public double getCombustivel () {
+        
+        return combustivel;
+
+    }
+
+    public void setCombustivel (double combustivel) {
+
+    }
+
+    public double getQuilometragem() {
+        
+        return combustivel;
+
+    }
+
+    public void setQuilometragem (double quilometros) {
+
+    }
+
+}
